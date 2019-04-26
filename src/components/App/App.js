240a7f9cf203a32df4215b13c0514e9f10bd8305
Alex <alexiv1102@gmail.com>
2019-04-26 15:38:08 +0300

    sort by status or gender

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,7 +8,8 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            list: []
+            list: [],
+            sort: []
         };
     }
 
@@ -24,7 +25,6 @@ class App extends Component {
                 status: item.status
             }
         })
-        // const {name, image, gender, location, species, status} = this.props.card;
 
         console.log(list);
         this.setState({list});
@@ -32,7 +32,18 @@ class App extends Component {
 
 
     sort = (e) => {
+        console.log(e.target)
+        console.log(this.state)
 
+        let sort;
+        if (e.target.value !== "All") {
+            sort = this.state.list.filter(card => card.status === e.target.value || card.gender === e.target.value);
+        } else {
+            sort = this.state.list;
+        }
+
+        console.log(sort)
+        this.setState({sort})
     }
 
 
